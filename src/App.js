3c93362b86f6eb495c5386b43c0aa89e816e5803
Adesiyan Temitope <adesiyantope2014@gmail.com>
2023-10-18 19:42:14 +0100

    latest

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 
 import './App.css';
+import Press from "./pages/Press";
 
 
 export default function App() {
@@ -10,6 +11,7 @@ export default function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
+      <Route path="/press" element={<Press />} />
     </Routes>
   );
 }
